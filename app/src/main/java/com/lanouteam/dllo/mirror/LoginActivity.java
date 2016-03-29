4ab0692e67de62c341220ff909381eb73dd821bf
谷秋爽 <1580002416@qq.com>
2016-03-29 19:54:51 +0800

    登录页面,button未设选择器

---
+++ app/src/main/java/com/lanouteam/dllo/mirror/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.lanouteam.dllo.mirror;
+
+import com.lanouteam.dllo.mirror.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/3/29.
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_login;
+    }
+
+
+    /**
+     * @param
+     *
+     * */
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
