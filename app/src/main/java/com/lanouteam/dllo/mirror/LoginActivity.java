5b66dd565f645aad40d119a0d2811b90a495bf7b
谷秋爽 <1580002416@qq.com>
2016-03-29 20:49:46 +0800

    登录页面二次提交

--- app/src/main/java/com/lanouteam/dllo/mirror/LoginActivity.java
+++ app/src/main/java/com/lanouteam/dllo/mirror/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.lanouteam.dllo.mirror;
 
+import android.view.View;
+import android.widget.ImageView;
+
 import com.lanouteam.dllo.mirror.base.BaseActivity;
 
 /**
  * Created by dllo on 16/3/29.
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    private ImageView deleteIv, weChatIv, weiboIv;
+
     @Override
     protected int getLayout() {
         return R.layout.activity_login;
@@ -14,15 +19,33 @@ public class LoginActivity extends BaseActivity {
 
     /**
      * @param
-     *
-     * */
+     */
     @Override
     protected void initData() {
-
+        deleteIv.setOnClickListener(this);
     }
 
     @Override
     protected void initView() {
+        deleteIv = bindView(R.id.login_activity_delete);
+        weiboIv = bindView(R.id.login_activity_weibo_iv);
+        weChatIv = bindView(R.id.login_activity_weChat_iv);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_activity_delete:
+                finish();
+                break;
+            case R.id.login_activity_weibo_iv:
+
+
+                break;
+            case R.id.login_activity_weChat_iv:
+
+                break;
+
+        }
     }
 }
